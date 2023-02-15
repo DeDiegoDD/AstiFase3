94a90221147da859bbcc530471a6e14fdc637268
Miguel de Diego Pérez <miosdediego@gmail.com>
2023-02-15 11:01:38 +0100

    V 2.0

--- Fase3.py
+++ Fase3.py
@@ -355,10 +355,9 @@ while r.leerT() < Tfin and not r.leerFinFases()   and not r.leerColision():
 
         else:
             # Si se está acercando a buen nivel continua hacia delante
-            if (prev_DD - DD) >= 0.17:
+            if (prev_DD - DD) >= 0.1758:
                 move(2, 0)
             
-        
             else:
                 if (DS > 0):
                     move(2, -1.8)
